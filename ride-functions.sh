3350c0691b84911130d15f3dd933b8ad5ed80de4
Sander <sander.contrib@gmail.com>
2024-01-26 18:48:15 +0000

    Fixed missing silence on curl

--- ride-functions.sh
+++ ride-functions.sh
@@ -508,7 +508,7 @@ InstallVisualStudioCode() {
     fi
 
     if [ ! -f /etc/apt/keyrings/microsoft.gpg ]; then
-        curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/keyrings/microsoft.gpg
+        curl -sS https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/keyrings/microsoft.gpg
         chmod 644 /etc/apt/keyrings/microsoft.gpg
     fi
     echo 'deb [signed-by=/etc/apt/keyrings/microsoft.gpg arch=amd64] https://packages.microsoft.com/repos/vscode stable main' > /etc/apt/sources.list.d/vscode.list
